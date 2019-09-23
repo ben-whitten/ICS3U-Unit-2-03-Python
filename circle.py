e294b9b87cf1b4f35824b8b904773fded6f1dae9
ben-whitten <benjamin.whitten@mths.ca>
2019-09-23 23:44:13 +0000

    added program

--- circle.py
+++ circle.py
@@ -2,23 +2,17 @@
 
 # Created by: Ben Whitten
 # Created on: September 2019
-# This is a program which can find the area and perimeter or a circle.
+# This is a program which can find the area of a circle.
 
-
-# This is what finds the area and perimeter of the circle.
-# Replace the two numbers at the end of lines 12, 14 and 15 with radius
-# to continue.
-import math
 
 import constants
 
-radius = 8
+radius = int(input("Input the radius of the circle:"))
 circumference = constants.TAU*radius
 
 
 def main():
-    print("If a circle has the radius:")
-    print("{}cm" .format(radius))
+    print("If a circle has the radius {}cm" .format(radius))
     print("")
     print("Then the area of the circle is {}cm^2" .format(circumference))
 
